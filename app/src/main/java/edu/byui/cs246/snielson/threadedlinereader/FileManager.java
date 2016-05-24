a47d72d71197b988fdac969db9fac1e21aa21a13
Stephen Nielson <stephen@nielson.org>
2016-05-24 11:38:22 -0600

    Switched to using async tasks.

--- app/src/main/java/edu/byui/cs246/snielson/threadedlinereader/FileManager.java
+++ app/src/main/java/edu/byui/cs246/snielson/threadedlinereader/FileManager.java
@@ -1,5 +1,6 @@
 package edu.byui.cs246.snielson.threadedlinereader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -15,105 +16,21 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager extends AppCompatActivity {
 
   public static final String NUMBERS_FILENAME = "numbers.txt";
   public static final String FILE_MANAGER_FQN = "edu.byui.cs246.snielson.threadedlinereader.FileManager";
+  public static final int MAX_NUMBERS = 10;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_file_manager);
-    this.setProgressBar(this, 0);
-  }
-
-  public void createFile(final View view) {
-    final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
-    final FileManager ptr = this;
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try (PrintWriter out = new PrintWriter(new FileWriter(file))) {
-          for (int i = 1; i <= 10; i++) {
-            out.println(i);
-            ptr.updateProgressBar(i, view);
-            try {
-              Thread.sleep(250);
-            } catch (InterruptedException ex) {
-              Log.wtf(FILE_MANAGER_FQN, ex);
-            }
-          }
-          // once it's done reset the counter
-          ptr.updateProgressBar(0, view);
-        } catch (IOException ex) {
-          Log.wtf(FILE_MANAGER_FQN, ex);
-        }
-      }
-    }).start();
-  }
-
-  public void loadFile(final View view) {
-    final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
-    final FileManager ptr = this;
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        if (!file.exists()) {
-          Log.wtf(FILE_MANAGER_FQN, "File should exist before file is read");
-          return;
-        }
-        final ArrayAdapter<String> numbers = new ArrayAdapter<>(ptr, android.R.layout.simple_list_item_1);
-        try {
-          int counter = 1;
-          LineIterator iterator = FileUtils.lineIterator(file);
-          while (iterator.hasNext()) {
-            numbers.add(iterator.nextLine());
-            try {
-              ptr.updateProgressBar(counter++, view);
-              Thread.sleep(250);
-            } catch (InterruptedException ex) {
-              Log.wtf(FILE_MANAGER_FQN, ex);
-            }
-          }
-        } catch (IOException ex) {
-          Log.wtf(FILE_MANAGER_FQN, "Could not read lines from file", ex);
-          ex.printStackTrace();
-        }
-        // now that we finished... let's update the progress bar
-        ptr.updateProgressBar(0, view);
-
-        // now update with our list
-        view.post(new Runnable() {
-          @Override
-          public void run() {
-            ListView listView = (ListView) ptr.findViewById(R.id.listView);
-            if (listView == null) {
-              throw new RuntimeException("Could not find listview");
-            }
-            listView.setAdapter(numbers);
-          }
-        });
-      }
-    }).start();
-  }
-
-  private void updateProgressBar(final int currentProgress, View view) {
-    final FileManager ptr = this;
-    view.post(new Runnable() {
-      @Override
-      public void run() {
-        ptr.setProgressBar(ptr, currentProgress);
-      }
-    });
-  }
-
-  private void setProgressBar(FileManager currentContext, int currentProgress) {
-    ProgressBar bar = (ProgressBar) currentContext.findViewById(R.id.progressBar);
-    if (bar == null) {
-      throw new RuntimeException("Could not find progress bar by id");
-    }
-    bar.setProgress(currentProgress);
+    setProgressBarMaxProgress(MAX_NUMBERS);
+    setProgressBar(0);
   }
 
   public void clearView(View v) {
@@ -123,5 +40,126 @@ public class FileManager extends AppCompatActivity {
       ArrayAdapter array = (ArrayAdapter) listView.getAdapter();
       array.clear();
     }
+  }
+
+  public void createFile(final View view) {
+    final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
+    CreateFileAsyncTask task = new CreateFileAsyncTask();
+    task.execute(file);
+  }
+
+  public void loadFile(final View view) {
+    final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
+    LoadFileAsyncTask task = new LoadFileAsyncTask();
+    task.execute(file);
+  }
+
+
+  private class LoadFileAsyncTask extends AsyncTask<File, Integer, List<String>> {
+    @Override
+    protected List<String> doInBackground(File... params) {
+      List<String> numbers = new ArrayList<>();
+      try {
+        int counter = 1;
+        LineIterator iterator = FileUtils.lineIterator(params[0]);
+        while (iterator.hasNext()) {
+          numbers.add(iterator.nextLine());
+          try {
+            publishProgress(counter++);
+            Thread.sleep(250);
+          } catch (InterruptedException ex) {
+            Log.wtf(FILE_MANAGER_FQN, ex);
+          }
+        }
+      } catch (IOException ex) {
+        Log.wtf(FILE_MANAGER_FQN, "Could not read lines from file", ex);
+        ex.printStackTrace();
+      }
+      // now that we finished... let's update the progress bar
+      publishProgress(0);
+      return numbers;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> readLines) {
+      super.onPostExecute(readLines);
+
+      if (isFinishing()) {
+        return; // do nothing if the view is terminated / being terminated.
+      }
+      setListViewData(readLines);
+    }
+
+
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+      super.onProgressUpdate(values);
+
+      setProgressBar(values[0]);
+    }
+  }
+
+  private class CreateFileAsyncTask extends AsyncTask<File, Integer, Void> {
+    @Override
+    protected Void doInBackground(File... params) {
+      File file = params[0];
+      try (PrintWriter out = new PrintWriter(new FileWriter(file))) {
+        for (int i = 1; i <= MAX_NUMBERS; i++) {
+          out.println(i);
+          publishProgress(i);
+          try {
+            Thread.sleep(250);
+          } catch (InterruptedException ex) {
+            Log.wtf(FILE_MANAGER_FQN, ex);
+          }
+        }
+        publishProgress(0);
+      } catch (IOException ex) {
+        Log.wtf(FILE_MANAGER_FQN, ex);
+      }
+      return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+      super.onProgressUpdate(values);
+      // avoid memory leaks and things being null as they are destroyed
+      // @see http://stackoverflow.com/questions/3033007/android-memory-leak-due-to-asynctask
+      if (isFinishing()) {
+        return;
+      }
+
+      ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+      if (bar == null) {
+        throw new RuntimeException("Could not find progress bar by id");
+      }
+      bar.setProgress(values[0]);
+    }
+  }
+
+  private void setListViewData(List<String> data) {
+    ArrayAdapter<String> numbers = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+    ListView listView = (ListView) findViewById(R.id.listView);
+    if (listView == null) {
+      throw new RuntimeException("Could not find listview");
+    }
+    listView.setAdapter(numbers);
+  }
+
+  private void setProgressBarMaxProgress(int maxProgress) {
+    ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
+    if (bar == null) {
+      throw new RuntimeException("Could not find progress bar by id");
+    }
+    bar.setMax(maxProgress);
+  }
+
+  private void setProgressBar(int currentProgress) {
+    ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
+    if (bar == null) {
+      throw new RuntimeException("Could not find progress bar by id");
+    }
+    bar.setProgress(currentProgress);
   }
 }
