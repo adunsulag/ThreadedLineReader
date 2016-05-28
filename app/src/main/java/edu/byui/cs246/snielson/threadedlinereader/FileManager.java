9a625d9641abfc04dc860c1ae8e37990177a1afe
Stephen Nielson <stephen@nielson.org>
2016-05-28 17:37:42 -0600

    Checkstyle updates.

--- app/src/main/java/edu/byui/cs246/snielson/threadedlinereader/FileManager.java
+++ app/src/main/java/edu/byui/cs246/snielson/threadedlinereader/FileManager.java
@@ -19,10 +19,24 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * File manager API for creating, reading, and displaying a list of numbers.
+ */
 public class FileManager extends AppCompatActivity {
 
+  /** The name of the file to save our list of 
+    * numbers to and read them from. 
+  **/
   public static final String NUMBERS_FILENAME = "numbers.txt";
-  public static final String FILE_MANAGER_FQN = "edu.byui.cs246.snielson.threadedlinereader.FileManager";
+  /**
+   * Name to use for reporting exceptions to the debug log.
+   */
+  public static final String FILE_MANAGER_FQN 
+      = "edu.byui.cs246.snielson.threadedlinereader.FileManager";
+  /**
+   * The highest number to be written out (from 1 to this number) 
+   * in our files and read from them.
+   */
   public static final int MAX_NUMBERS = 10;
 
   @Override
@@ -33,7 +47,10 @@ public class FileManager extends AppCompatActivity {
     setProgressBar(0);
   }
 
-  public void clearView(View v) {
+  /** Button action to clear the list if there is one.
+    * @param view the button that was clicked on
+    **/
+  public void clearView(View view) {
     ListView listView = (ListView) this.findViewById(R.id.listView);
     if (listView != null && listView.getAdapter() != null
         && listView.getAdapter() instanceof ArrayAdapter) {
@@ -42,12 +59,19 @@ public class FileManager extends AppCompatActivity {
     }
   }
 
+  /** Button action to create a file on the android 
+    * filesystem with a list of numbers.
+    * @param view the button that was clicked on
+    **/
   public void createFile(final View view) {
     final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
     CreateFileAsyncTask task = new CreateFileAsyncTask();
     task.execute(file);
   }
 
+  /** Button action to load a list of items on the gui screen from the filesystem.
+    * @param view the button that was clicked on
+    **/
   public void loadFile(final View view) {
     final File file = new File(view.getContext().getFilesDir(), NUMBERS_FILENAME);
     LoadFileAsyncTask task = new LoadFileAsyncTask();
@@ -55,6 +79,10 @@ public class FileManager extends AppCompatActivity {
   }
 
 
+  /**
+   * Loads a file and returns that file as a list of strings with one string per line.
+   * The task also gives status updates as it progresses.
+   */
   private class LoadFileAsyncTask extends AsyncTask<File, Integer, List<String>> {
     @Override
     protected List<String> doInBackground(File... params) {
@@ -100,6 +128,10 @@ public class FileManager extends AppCompatActivity {
     }
   }
 
+  /**
+   * Creates a file at the given location with a list of numbers with 1 number per line.
+   * The task also gives status updates as it progresses.
+   */
   private class CreateFileAsyncTask extends AsyncTask<File, Integer, Void> {
     @Override
     protected Void doInBackground(File... params) {
@@ -138,8 +170,13 @@ public class FileManager extends AppCompatActivity {
     }
   }
 
+  /**
+   * Given a list of strings it displays that list of strings in our list view
+   * @param data The list of strings to display on the screen in the list view.
+   */
   private void setListViewData(List<String> data) {
-    ArrayAdapter<String> numbers = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+    ArrayAdapter<String> numbers 
+        = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
     ListView listView = (ListView) findViewById(R.id.listView);
     if (listView == null) {
       throw new RuntimeException("Could not find listview");
@@ -147,6 +184,10 @@ public class FileManager extends AppCompatActivity {
     listView.setAdapter(numbers);
   }
 
+  /**
+   * Sets the maximum value the progress bar can be.
+   * @param maxProgress The maximum integer value that the progress bar can progress to.
+   */
   private void setProgressBarMaxProgress(int maxProgress) {
     ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
     if (bar == null) {
@@ -155,6 +196,10 @@ public class FileManager extends AppCompatActivity {
     bar.setMax(maxProgress);
   }
 
+  /**
+   * Sets the current progress value of the progress bar.
+   * @param currentProgress The current value of the progress bar.
+   */
   private void setProgressBar(int currentProgress) {
     ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
     if (bar == null) {
